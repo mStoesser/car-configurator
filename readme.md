92db766b0a18618da2a111b293a36fe7b0da8cb7
Michael Stößer <michael.stoesser@nexmart.com>
2023-09-21 10:16:59 +0200

    add readme, update cors todo

--- readme.md
+++ readme.md
@@ -1,9 +1,16 @@
+# Car Configurator
 
 
-### Docker build
+## Build
+Use JDK 17 (for example with sdkman: https://sdkman.io/ `sdk use java 17.0.3-tem`)
+
 ```
+mvn clean install
 docker build -t de.mbti/carconfigurator .
+```
 
+### Run
+```
 docker run -p 8080:8080 de.mbti/carconfigurator
 ```
 
--- src/main/kotlin/de/mbti/carconfigurator/resource/CarConfigurationResource.kt
+++ src/main/kotlin/de/mbti/carconfigurator/resource/CarConfigurationResource.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("http://localhost:3000") //TODO: use a global cors config
 @RequestMapping("/api/configuration", produces = [MediaType.APPLICATION_JSON_VALUE])
 class CarConfigurationResource(
     val carConfigurationService: CarConfigurationService,
